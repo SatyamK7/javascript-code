54f6455d1ca53ca8b3b79b4249e3acc2c8b8b1d1
SatyamK7 <145842450+SatyamK7@users.noreply.github.com>
2024-04-13 05:36:06 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = true
+
+let valueInNumber = Number(score)
+// console.log(typeof score)
+// console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//
+let isLoggedIn = 1
+let boolLogged = Boolean(isLoggedIn)
+console.log(typeof isLoggedIn)
+console.log(boolLogged)
+
+// 1 -> in boolean is true and similarly 0 for false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
