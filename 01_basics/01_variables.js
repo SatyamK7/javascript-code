7f97bb3d1af65358adf706d6ea8599f8920534e0
SatyamK7 <145842450+SatyamK7@users.noreply.github.com>
2024-04-13 04:07:12 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 3434
+let accEmail = "trhfuhga@gmail.com"
+var accPassword = "hgfhhbdjh" // Prefer not to use because of issues in block scope and functional scope
+accountCity = "Siwan"
+let accountState;
+
+
+//accountId = 2 // a constant variable can not be updated
+console.log(accountState)
+
+console.table([accEmail,accountId,accPassword,accountCity,accountState])
