d4ff878ab77aa21dad582fc1b44a7977b36fc57f
SatyamK7 <145842450+SatyamK7@users.noreply.github.com>
2024-04-13 04:05:29 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict" // treat all JS code as newer version of javascript
+
+// alert("Hello") // we are using nodejs not browser
+
+// console.log(3 + 3)
+// console.log("Kumar")
+
+let name = "Satyam"
+let age = 21
+let isStudent = true
+
+console.log(typeof null)
